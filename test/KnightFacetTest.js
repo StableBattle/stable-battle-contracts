0c1ff52ba6332471b062b833c7b7826478d62668
Pavel Ivanov <zeuamsa@gmail.com>
2022-07-15 01:58:48 +0400

    made it more universal

--- test/KnightFacetTest.js
+++ test/KnightFacetTest.js
@@ -11,6 +11,7 @@ describe('KnightFacetTest', async function () {
   let knightPrice = { USDT: 1e9, USDC: 1e9, TEST: 0}
   let COIN = []
   let numberOfCoins = 2;
+  let numberOfPools = 1;
   let owner
   let user1
   let user2
@@ -76,14 +77,14 @@ describe('KnightFacetTest', async function () {
   })
 
   it('check that Coins work alright', async () => {
-    for (let i = 1; i < numberOfCoins + 1; i++) {
-      let price = await await SBD.KnightFacet.getKnightPrice(i)
-      let balance_before = await COIN[i].balanceOf(owner.address)
-      await COIN[i].mint(price)
-      let balance_after = await COIN[i].balanceOf(owner.address)
+    for (let c = 1; c < numberOfCoins + 1; c++) {
+      let price = await await SBD.KnightFacet.getKnightPrice(c)
+      let balance_before = await COIN[c].balanceOf(owner.address)
+      await COIN[c].mint(price)
+      let balance_after = await COIN[c].balanceOf(owner.address)
       assert.equal(balance_after - balance_before, price)
-      await COIN[i].approve(SBD.Address, price)
-      let allowance = await COIN[i].allowance(owner.address, SBD.Address)
+      await COIN[c].approve(SBD.Address, price)
+      let allowance = await COIN[c].allowance(owner.address, SBD.Address)
       expect(allowance).to.equal(price)
     }
   })
@@ -92,75 +93,53 @@ describe('KnightFacetTest', async function () {
   //mintKnight(1, 2) = mint AAVE USDC
   //mintKnight(2, 3) = mint TEST TEST
 
-  it('should mint a knight correctly for AAVE USDT', async () => {
-    await SBD.KnightFacet.mintKnight(1, 1)
-    let eventsKnightMinted = await SBD.KnightFacet.queryFilter('KnightMinted')
-    let [knightId, knightOwner, knightPool, KnightCoin] = eventsKnightMinted[0].args
-    expect(knightOwner).to.equal(owner.address)
-    expect(knightPool).to.equal(1)
-    expect(KnightCoin).to.equal(1)
-    let knightOwnerFromCall = await SBD.KnightFacet.getKnightOwner(knightId)
-    expect(knightOwner).to.equal(knightOwnerFromCall)
-    let knightPoolFromCall = await SBD.KnightFacet.getKnightPool(knightId)
-    expect(knightPool).to.equal(knightPoolFromCall)
-    let knightCoinFromCall = await SBD.KnightFacet.getKnightCoin(knightId)
-    expect(KnightCoin).to.equal(knightCoinFromCall)
+  it('should mint a knight correctly for all valid combinations of Pool and Coin', async () => {
+    for(let p = 1; p < numberOfPools + 1; p++) {
+      for(let c = 1; c < numberOfCoins + 1; c++) {
+        if (await SBD.KnightFacet.getPoolAndCoinCompatibility(p, c)) {
+          let eventCount = (c - 1) + (p - 1) * numberOfCoins
+          await SBD.KnightFacet.mintKnight(p, c)
+          let eventsKnightMinted = await SBD.KnightFacet.queryFilter('KnightMinted')
+          let [knightId, knightOwner, knightPool, KnightCoin] = eventsKnightMinted[eventCount].args
+          expect(knightOwner).to.equal(owner.address)
+          expect(knightPool).to.equal(p)
+          expect(KnightCoin).to.equal(c)
+          let knightOwnerFromCall = await SBD.KnightFacet.getKnightOwner(knightId)
+          expect(knightOwner).to.equal(knightOwnerFromCall)
+          let knightPoolFromCall = await SBD.KnightFacet.getKnightPool(knightId)
+          expect(knightPool).to.equal(knightPoolFromCall)
+          let knightCoinFromCall = await SBD.KnightFacet.getKnightCoin(knightId)
+          expect(KnightCoin).to.equal(knightCoinFromCall)
+        }
+      }
+    }
   })
 
-  it('should burn a knight correctly and return USDT from AAVE', async () => {
-    let eventsKnightMinted = await SBD.KnightFacet.queryFilter('KnightMinted')
-    let [knightId, knightOwner, knightPool, KnightCoin] = eventsKnightMinted[0].args
-    
-    let balance_before = await USDT.balanceOf(owner.address)
-    await SBD.KnightFacet.burnKnight(knightId)
-    let balance_after = await USDT.balanceOf(owner.address)
-    expect(balance_after - balance_before).to.equal(await SBD.KnightFacet.getKnightPrice(1))
-    let eventsKnightBurned = await SBD.KnightFacet.queryFilter('KnightBurned')
-    let [knightId2, knightOwner2, knightPool2, KnightCoin2] = eventsKnightBurned[0].args
-    expect(knightId).to.equal(knightId2)
-    expect(knightOwner).to.equal(knightOwner2)
-    expect(knightPool).to.equal(knightPool2)
-    expect(KnightCoin).to.equal(KnightCoin2)
+  it('should burn a knight correctly for all valid combinations of Pool and Coin', async () => {
+    for(let p = 1; p < numberOfPools + 1; p++) {
+      for(let c = 1; c < numberOfCoins + 1; c++) {
+        if (await SBD.KnightFacet.getPoolAndCoinCompatibility(p, c)) {
+          let eventCount = (c - 1) + (p - 1) * numberOfCoins
+          let eventsKnightMinted = await SBD.KnightFacet.queryFilter('KnightMinted')
+          let [knightId, knightOwner, knightPool, KnightCoin] = eventsKnightMinted[eventCount].args
+          
+          let balance_before = await COIN[c].balanceOf(owner.address)
+          await SBD.KnightFacet.burnKnight(knightId)
+          let balance_after = await COIN[c].balanceOf(owner.address)
+          expect(balance_after - balance_before).to.equal(await SBD.KnightFacet.getKnightPrice(c))
+          let eventsKnightBurned = await SBD.KnightFacet.queryFilter('KnightBurned')
+          let [knightId2, knightOwner2, knightPool2, KnightCoin2] = eventsKnightBurned[eventCount].args
+          expect(knightId).to.equal(knightId2)
+          expect(knightOwner).to.equal(knightOwner2)
+          expect(knightPool).to.equal(knightPool2)
+          expect(KnightCoin).to.equal(KnightCoin2)
 
-    let knightInfo = await SBD.KnightFacet.getKnightInfo(knightId)
-    expect(knightInfo.pool).to.equal(0)
-    expect(knightInfo.coin).to.equal(0)
-    expect(knightInfo.owner).to.equal(ethers.constants.AddressZero)
-  })
-
-  it('should mint a knight correctly for AAVE USDC', async () => {
-    await SBD.KnightFacet.mintKnight(1, 2)
-    let eventsKnightMinted = await SBD.KnightFacet.queryFilter('KnightMinted')
-    let [knightId, knightOwner, knightPool, KnightCoin] = eventsKnightMinted[1].args
-    expect(knightOwner).to.equal(owner.address)
-    expect(knightPool).to.equal(1)
-    expect(KnightCoin).to.equal(2)
-    let knightOwnerFromCall = await SBD.KnightFacet.getKnightOwner(knightId)
-    expect(knightOwner).to.equal(knightOwnerFromCall)
-    let knightPoolFromCall = await SBD.KnightFacet.getKnightPool(knightId)
-    expect(knightPool).to.equal(knightPoolFromCall)
-    let knightCoinFromCall = await SBD.KnightFacet.getKnightCoin(knightId)
-    expect(KnightCoin).to.equal(knightCoinFromCall)
-  })
-
-  it('should burn a knight correctly and return USDC from AAVE', async () => {
-    let eventsKnightMinted = await SBD.KnightFacet.queryFilter('KnightMinted')
-    let [knightId, knightOwner, knightPool, KnightCoin] = eventsKnightMinted[1].args
-    
-    let balance_before = await USDC.balanceOf(owner.address)
-    await SBD.KnightFacet.burnKnight(knightId)
-    let balance_after = await USDC.balanceOf(owner.address)
-    expect(balance_after - balance_before).to.equal(await SBD.KnightFacet.getKnightPrice(1))
-    let eventsKnightBurned = await SBD.KnightFacet.queryFilter('KnightBurned')
-    let [knightId2, knightOwner2, knightPool2, KnightCoin2] = eventsKnightBurned[1].args
-    expect(knightId).to.equal(knightId2)
-    expect(knightOwner).to.equal(knightOwner2)
-    expect(knightPool).to.equal(knightPool2)
-    expect(KnightCoin).to.equal(KnightCoin2)
-
-    let knightInfo = await SBD.KnightFacet.getKnightInfo(knightId)
-    expect(knightInfo.pool).to.equal(0)
-    expect(knightInfo.coin).to.equal(0)
-    expect(knightInfo.owner).to.equal(ethers.constants.AddressZero)
+          let knightInfo = await SBD.KnightFacet.getKnightInfo(knightId)
+          expect(knightInfo.pool).to.equal(0)
+          expect(knightInfo.coin).to.equal(0)
+          expect(knightInfo.owner).to.equal(ethers.constants.AddressZero)
+        }
+      }
+    }
   })
 })
