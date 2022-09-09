5c232a00a854d52f72e55d22785fe2a179c231a1
Pavel Ivanov <zeuamsa@gmail.com>
2022-09-09 19:42:18 +0400

    updated dummy

--- contracts/StableBattle/dummies/DemoFightFacetDummy.sol
+++ contracts/StableBattle/dummies/DemoFightFacetDummy.sol
@@ -1,27 +1,25 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 
-import { Pool, Coin } from "../storage/MetaStorage.sol";
-
 contract DemoFightFacetDummy {
 
-  function battleWonBy(address user, Pool pool, Coin coin, uint256 reward) external {}
+  function battleWonBy(address user, uint256 reward) public {}
 
-  function claimReward(address user, Pool pool, Coin coin) external {}
+  function claimReward(address user) public {}
 
 //External getters
 
-  function getTotalYield(Pool pool, Coin coin) external view returns(uint256) {}
+  function getTotalYield() external view returns(uint256) {}
 
-  function getCurrentYield(Pool pool, Coin coin) external view returns(uint256) {}
+  function getCurrentYield() external view returns(uint256) {}
 
-  function getLockedYield(Pool pool, Coin coin) external view returns(uint256) {}
+  function getLockedYield() external view returns(uint256) {}
 
-  function getStakedByKnights(Pool pool, Coin coin) external view returns(uint256) {}
+  function getStakedByKnights() external view returns(uint256) {}
 
-  function getUserReward(address user, Pool pool, Coin coin) external view returns(uint256) {}
+  function getUserReward(address user) external view returns(uint256) {}
 
-  function getYieldInfo(Pool pool, Coin coin)
+  function getYieldInfo()
     external
     view
     returns(uint256, uint256, uint256, uint256)
