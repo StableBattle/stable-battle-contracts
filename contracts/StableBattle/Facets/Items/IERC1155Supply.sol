f1d80109481b9bd5c2467aa5ea8d4dfbe82ecc66
Pavel Ivanov <zeuamsa@gmail.com>
2023-04-11 23:38:51 +0400

    Replaced ERC1155 module with the old one

---
+++ contracts/StableBattle/Facets/Items/IERC1155Supply.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/interfaces/IERC1155.sol";
+
+interface IERC1155Supply is IERC1155 {
+  function totalSupply(uint256 id) external view returns (uint256);
+  function exists(uint256 id) external view returns (bool);
+}
---
+++ contracts/StableBattle/Facets/Items/ItemsStorage.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.0;
+
+library ItemsStorage {
+  struct State {
+  //Original ERC1155 State
+    // Mapping from token ID to account balances
+    mapping(uint256 => mapping(address => uint256)) _balances;
+
+    // Mapping from account to operator approvals
+    mapping(address => mapping(address => bool)) _operatorApprovals;
+
+    // Used as the URI for all token types by relying on ID substitution, e.g. https://token-cdn-domain/{id}.json
+    string _uri;
+
+  //ERC1155Supply Addition
+    // Total amount of tokens in with a given id.
+    mapping(uint256 => uint256) _totalSupply;
+    
+  //Items Facet Addition
+    // Mapping from token ID to its owner
+    mapping (uint256 => address) _knightOwners;
+  }
+
+  bytes32 internal constant STORAGE_SLOT = keccak256("Items.storage");
+
+  function state() internal pure returns (State storage l) {
+    bytes32 slot = STORAGE_SLOT;
+    assembly {
+      l.slot := slot
+    }
+  }
+}
